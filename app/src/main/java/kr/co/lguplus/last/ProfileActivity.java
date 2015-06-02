5f9632525c73ddf50128cad4ca71deadd2ab3ff3
we <fintano@nate.com>
2015-06-03 01:29:21 +0900

    6/3 0128

--- app/src/main/java/kr/co/lguplus/last/ProfileActivity.java
+++ app/src/main/java/kr/co/lguplus/last/ProfileActivity.java
@@ -2,15 +2,29 @@ package kr.co.lguplus.last;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Random;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class ProfileActivity extends Activity {
+
+    private final int MAX = 99999999;
+    private final int MIN = 10000000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +41,26 @@ public class ProfileActivity extends Activity {
         String str;
         int whichAct = intent.getIntExtra("EXTRA_INT",1);
 
-        //nameView.setText(currentUser.name + " «–ª˝");
-        //nameView.setText(currentUser.\
+        // barcode image
+        Bitmap bitmap = null;
+        ImageView iv = (ImageView) findViewById(R.id.profile_barcode);
+        //make Random number for Barcode
+        Random rd = new Random();
+        int barcodeNum = rd.nextInt(MAX - MIN)+MIN;
+        nameView.setText(currentUser.name + " ÌïôÏÉù");
 
-        //);
+        //if it is went through PayActivity
         if(whichAct == UserInfo.FROM_PAY) {
-            str = currentUser.RRname + "\n" + currentUser.fromDate + " ~ " + currentUser.toDate + "\n Seat.No" + currentUser.seatNo;
+            str = currentUser.RRname + "\n" + currentUser.fromDate + " ~ " + currentUser.toDate + "\n Seat.No " + currentUser.seatNo;
+            // barcode creation
+            try {
+                bitmap = encodeAsBitmap(Integer.toString(barcodeNum), BarcodeFormat.CODE_128, 600, 300);
+                iv.setImageBitmap(bitmap);
+
+            } catch (WriterException e) {
+                e.printStackTrace();
+            }
+
             infoView.setGravity(View.TEXT_ALIGNMENT_CENTER);
             infoView.setText(str);
         }
@@ -67,5 +95,62 @@ public class ProfileActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**************************************************************
+     * getting from com.google.zxing.client.android.encode.QRCodeEncoder
+     *
+     * See the sites below
+     * http://code.google.com/p/zxing/
+     * http://code.google.com/p/zxing/source/browse/trunk/android/src/com/google/zxing/client/android/encode/EncodeActivity.java
+     * http://code.google.com/p/zxing/source/browse/trunk/android/src/com/google/zxing/client/android/encode/QRCodeEncoder.java
+     */
+
+    private static final int WHITE = 0xFFFFFFFF;
+    private static final int BLACK = 0xFF000000;
+
+    Bitmap encodeAsBitmap(String contents, BarcodeFormat format, int img_width, int img_height) throws WriterException {
+        String contentsToEncode = contents;
+        if (contentsToEncode == null) {
+            return null;
+        }
+        Map<EncodeHintType, Object> hints = null;
+        String encoding = guessAppropriateEncoding(contentsToEncode);
+        if (encoding != null) {
+            hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+            hints.put(EncodeHintType.CHARACTER_SET, encoding);
+        }
+        MultiFormatWriter writer = new MultiFormatWriter();
+        BitMatrix result;
+        try {
+            result = writer.encode(contentsToEncode, format, img_width, img_height, hints);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height,
+                Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
+    private static String guessAppropriateEncoding(CharSequence contents) {
+        // Very crude at the moment
+        for (int i = 0; i < contents.length(); i++) {
+            if (contents.charAt(i) > 0xFF) {
+                return "UTF-8";
+            }
+        }
+        return null;
     }
 }
