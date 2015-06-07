1b0bab21e9aba7d3968515f1ef3c41bb2ab88c98
we <fintano@nate.com>
2015-06-08 05:03:04 +0900

    6/8 0502

--- app/src/main/java/kr/co/lguplus/last/ProfileActivity.java
+++ app/src/main/java/kr/co/lguplus/last/ProfileActivity.java
@@ -1,15 +1,26 @@
 package kr.co.lguplus.last;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -25,21 +36,23 @@ public class ProfileActivity extends Activity {
 
     private final int MAX = 99999999;
     private final int MIN = 10000000;
-
+    private ReadingRoomStruct currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile);
+        setContentView(R.layout.barcode);
 
-        ReadingRoomStruct currentUser = UserInfo.getUserInfo();
-        Button menubtn = (Button) findViewById(R.id.profile_button);
-        TextView nameView = (TextView) findViewById(R.id.profile_name);
-        TextView infoView = (TextView) findViewById(R.id.profile_roominfo);
-
+        currentUser = UserInfo.getUserInfo();
+        ImageButton menubtn = (ImageButton) findViewById(R.id.barcode_button_menu);
+        TextView nameView = (TextView) findViewById(R.id.barcode_edit_name);
+        TextView infoView;
+        Button selectButton;
+        LinearLayout layout = (LinearLayout) findViewById(R.id.barcode_mainlayout);
         Intent intent = new Intent(this.getIntent());
         //Bundle bundle = intent.getExtras();
         String str;
-        int whichAct = intent.getIntExtra("EXTRA_INT",1);
+
+        int whichAct = intent.getIntExtra("EXTRA_INT",100);
 
         // barcode image
         Bitmap bitmap = null;
@@ -51,7 +64,6 @@ public class ProfileActivity extends Activity {
 
         //if it is went through PayActivity
         if(whichAct == UserInfo.FROM_PAY) {
-            str = currentUser.RRname + "\n" + currentUser.fromDate + " ~ " + currentUser.toDate + "\n Seat.No " + currentUser.seatNo;
             // barcode creation
             try {
                 bitmap = encodeAsBitmap(Integer.toString(barcodeNum), BarcodeFormat.CODE_128, 600, 300);
@@ -60,15 +72,22 @@ public class ProfileActivity extends Activity {
             } catch (WriterException e) {
                 e.printStackTrace();
             }
-
-            infoView.setGravity(View.TEXT_ALIGNMENT_CENTER);
-            infoView.setText(str);
+            infoView = new TextView(this);
+            fromSeatEvent(infoView);
+            layout.addView(infoView);
+        }
+        //if it is went through LoginActivity
+        else if(whichAct == UserInfo.FROM_LOGIN){
+            selectButton = new Button(this);
+            // set button's property
+            fromLoginEvent(selectButton);
+            layout.addView(selectButton);
         }
 
         menubtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent mapactivity = new Intent(ProfileActivity.this, SearchDemoActivity.class);
+                Intent mapactivity = new Intent(ProfileActivity.this, MenuActivity.class);
                 startActivity(mapactivity);
                 finish();
             }
@@ -152,5 +171,53 @@ public class ProfileActivity extends Activity {
             }
         }
         return null;
+    }
+    private void fromLoginEvent(Button selectButton){
+
+        float dpInPx;
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.CENTER_HORIZONTAL;
+        params.topMargin = 20;
+        selectButton.setLayoutParams(params);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        // get pixel value from dp
+        dpInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, dm); //300dp
+        selectButton.setWidth((int)dpInPx);
+        dpInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, dm);  //50dp
+        selectButton.setHeight((int)dpInPx);
+        selectButton.setText("독서실을 선택해주세요");
+        selectButton.setBackgroundColor(Color.parseColor("#ec068d"));
+        selectButton.setTextColor(Color.WHITE);
+        selectButton.setTextSize(25);
+        selectButton.setTypeface(null, Typeface.BOLD);
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regactivity = new Intent(ProfileActivity.this, SearchDemoActivity.class);
+                startActivity(regactivity);
+                finish();
+            }
+        });
+
+    }
+    private void fromSeatEvent(TextView infoView){
+
+        float dpInPx;
+
+        String str = "      " + currentUser.RRname + "\n" + currentUser.fromDate + " ~ " + currentUser.toDate + "\n Seat.No " + currentUser.seatNo;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.CENTER_HORIZONTAL;
+        params.topMargin = 20;
+        infoView.setLayoutParams(params);
+        infoView.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        infoView.setTextColor(Color.BLACK);
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        // get pixel value from dp
+        dpInPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 250, dm); //250dp
+        infoView.setWidth((int)dpInPx);
+        infoView.setTextSize(25);
+        infoView.setTypeface(null, Typeface.BOLD);
+        infoView.setText(str);
     }
 }
